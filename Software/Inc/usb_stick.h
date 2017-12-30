68b9f9f926cf43884355c85a3c37bcbc2d6a2015
gerdb <gerd@aline-gerd.de>
2017-12-30 20:14:44 +0100

    Import *.WAV file into wave table.

--- Software/Inc/usb_stick.h
+++ Software/Inc/usb_stick.h
@@ -24,6 +24,26 @@
 #ifndef USB_STICK_H_
 #define USB_STICK_H_
 
+/* Types ---------------------------------------------------------------- */
+typedef struct
+{
+  uint32_t   ChunkID;       /* 0 */
+  uint32_t   FileSize;      /* 4 */
+  uint32_t   FileFormat;    /* 8 */
+  uint32_t   SubChunk1ID;   /* 12 */
+  uint32_t   SubChunk1Size; /* 16*/
+  uint16_t   AudioFormat;   /* 20 */
+  uint16_t   NbrChannels;   /* 22 */
+  uint32_t   SampleRate;    /* 24 */
+
+  uint32_t   ByteRate;      /* 28 */
+  uint16_t   BlockAlign;    /* 32 */
+  uint16_t   BitPerSample;  /* 34 */
+  uint32_t   SubChunk2ID;   /* 36 */
+  uint32_t   SubChunk2Size; /* 40 */
+
+}WAVE_FormatTypeDef;
+
 /* Function prototypes ----------------------------------------------------- */
 void USB_STICK_Connected(void);
 void USB_STICK_Disconnected(void);
--- Software/Src/usb_stick.c
+++ Software/Src/usb_stick.c
@@ -25,6 +25,7 @@
 #include "fatfs.h"
 #include "usb_host.h"
 #include "theremin.h"
+#include "usb_stick.h"
 
 /* Variable used by FatFs*/
 int bMounted = 0;
@@ -34,7 +35,7 @@ uint32_t ulBytesRead;
 
 /* Function prototypes for local functions ------------------------------------ */
 static void USB_STICK_ReadCFile(char* filename);
-
+static void USB_STICK_ReadWAVFile(char* filename);
 static int USB_STICK_ParseNumber(int* number, const char* line);
 
 /* Local functions ------------------------------------------------------------ */
@@ -83,7 +84,6 @@ static void USB_STICK_ReadCFile(char* filename)
 {
 	int value;
 	int cnt = 0;
-	int i;
 	int32_t l;
 
 	// File on stick?
@@ -108,14 +108,65 @@ static void USB_STICK_ReadCFile(char* filename)
 			}
 		}
 
-		// At least one number was vaild?
+		// At least one number was valid?
 		if (cnt >= 1)
 		{
 			// Fill the rest of the table by repeating the content
-			for (i = cnt; i < 4096; i++)
+			for (int i = cnt; i < 4096; i++)
 			{
 				ssWaveTable[i] = ssWaveTable[i - cnt];
 			}
+		}
+	}
+}
+
+/**
+ * @brief Read an Microsoft *.wav audio file
+ * @param filename: Filename to read
+ */
+static void USB_STICK_ReadWAVFile(char* filename)
+{
+	WAVE_FormatTypeDef waveformat;
+	unsigned int ulBytesread = 0;
+	uint32_t ulWaveDataLength = 0;
+	int16_t ssWavSample, ssLastWavSample;
+	int iWaveCnt = 0;
+	int iWaveSamplePos, iLastWaveSamplePos;
+
+	// Open the Wave file to be played
+	if (f_open(&USBHFile, filename, FA_READ) == FR_OK)
+	{
+		// Read sizeof(WaveFormat) from the selected file
+		f_read(&USBHFile, &waveformat, sizeof(waveformat), &ulBytesread);
+
+		// Set WaveDataLenght to the Speech Wave length
+		ulWaveDataLength = waveformat.SubChunk2Size / 2;
+
+		iWaveCnt = 0;
+		ssLastWavSample = 0;
+		iLastWaveSamplePos = 0;
+		// Read all samples
+		for (int i = 0; i < ulWaveDataLength && i < 1024; i++)
+		{
+			// Read one sample
+			f_read(&USBHFile, &ssWavSample, 2, &ulBytesread);
+			iWaveSamplePos = (i * 1024) / ulWaveDataLength;
+			for (; iWaveCnt < iWaveSamplePos; iWaveCnt++)
+			{
+				//Interpolate to scale e.g. 160 sample from a WAV file to 1024 samples in the wave table
+				ssWaveTable[iWaveCnt] = (ssWavSample - ssLastWavSample)
+						* (iWaveCnt - iLastWaveSamplePos)
+						/ (iWaveSamplePos - iLastWaveSamplePos)
+						+ ssLastWavSample;
+			}
+			ssLastWavSample = ssWavSample;
+			iLastWaveSamplePos = iWaveSamplePos;
+		}
+
+		// Fill the rest of the table by repeating the content
+		for (int i = 1024; i < 4096; i++)
+		{
+			ssWaveTable[i] = ssWaveTable[i - 1024];
 		}
 	}
 }
@@ -129,7 +180,8 @@ void USB_STICK_Connected(void)
 	if (f_mount(&USBHFatFS, (TCHAR const*) USBHPath, 0) == FR_OK)
 	{
 		bMounted = 1;
-		USB_STICK_ReadCFile("WAV1.C");
+		//USB_STICK_ReadCFile("WAV1.C");
+		USB_STICK_ReadWAVFile("WAV1.WAV");
 	}
 }
 
